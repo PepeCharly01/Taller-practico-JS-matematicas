8ff7fdd30287ae3e61de6efa2a8e78ca47ac69d5
PepeCharly01 <jc.espinosa2@gmail.com>
2022-11-24 18:15:28 -0600

    proyeccion por persona, creamos una función para saber el proximo salario que puede tener una persona

--- analisis.js
+++ analisis.js
@@ -15,3 +15,27 @@ function medianaPorPersona(nombrePersona) {
     console.log(medianaSalarios);
     return medianaSalarios;
 }
+
+function proyeccionPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+    let porcentajesCrecimiento = [];
+
+    for (let i = 1; i < trabajos.length; i++) {
+        const salarioActual = trabajos[i].salario;
+        const salarioPasado = trabajos[i - 1].salario;
+        const crecimiento = salarioActual - salarioPasado;
+        const porcentajeCrecimiento = (crecimiento / salarioPasado);
+        porcentajesCrecimiento.push(porcentajeCrecimiento);
+    }
+
+    const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+
+    // console.log({porcentajesCrecimiento, medianaPorcentajesCrecimiento});
+
+    const ultimoSalario = trabajos[trabajos.length - 1].salario;
+    const aumento = ultimoSalario * medianaPorcentajesCrecimiento;
+    const nuevoSalario = ultimoSalario + aumento;
+
+    return nuevoSalario;
+}
